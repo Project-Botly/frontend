598a7199b1ed791cbc36140951170868a87a8f34
Hector Tamayo <hectormtg@gmail.com>
2025-09-07 13:52:46 -0400

    simple form to test meta signup flow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,127 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+
+import { API_URL } from '@/constants/api.constants'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import styles from './page.module.css'
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [payload, setPayload] = useState({
+    name: '',
+    industry: '',
+    email: '',
+    phoneNumber: '',
+  })
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+  const handleChange = (value = {}) => {
+    setPayload((prev) => ({ ...prev, ...value }))
+  }
+
+  useEffect(() => {
+    // SDK initialization
+    if (!window?.fbAsyncInit) return
+    window.fbAsyncInit = () => {
+      window.FB.init({
+        appId: '1822695871933811',
+        autoLogAppEvents: true,
+        xfbml: true,
+        version: 'v22.0',
+      })
+    }
+
+    window.addEventListener('message', (event) => {
+      if (!event.origin.endsWith('facebook.com')) return
+      try {
+        const data = JSON.parse(event.data)
+        if (data.type === 'WA_EMBEDDED_SIGNUP') {
+          console.log('message event: ', data)
+          /**
+           *  data: {
+                phone_number_id: '<CUSTOMER_BUSINESS_PHONE_NUMBER_ID>',
+                waba_id: '<CUSTOMER_WABA_ID>',
+                business_id: '<CUSTOMER_BUSINESS_PORTFOLIO_ID>'
+              }
+           */
+          const { phone_number_id, waba_id } = data.data
+          axios.post(`${API_URL}/business/configure`, {
+            phoneNumberId: phone_number_id,
+            wabaId: waba_id,
+            name: payload.name,
+            industry: payload.industry,
+          })
+        }
+      } catch (err) {
+        console.error('Error on message listener: ', err)
+        console.log('message event: ', event.data)
+        // your code goes here
+      }
+    })
+  }, [])
+
+  const fbLoginCallback = (response: any) => {
+    if (response.authResponse) {
+      const code = response.authResponse.code
+      console.log('response: ', code) // remove after testing
+      // your code goes here
+    } else {
+      console.log('response: ', response) // remove after testing
+      // your code goes here
+    }
+  }
+
+  const launchWhatsAppSignup = () => {
+    window.FB.login(fbLoginCallback, {
+      config_id: '803183145621177', // your configuration ID goes here
+      response_type: 'code',
+      override_default_response_type: true,
+      extras: {
+        setup: {},
+        // featureType: '<FEATURE_TYPE>', // default onboarding
+        sessionInfoVersion: '3',
+      },
+    })
+  }
+
+  return (
+    <>
+      <div className={styles.page}>
+        <main className={styles.main}>
+          <form className={styles.form}>
+            <input
+              placeholder="Business name"
+              value={payload.name}
+              onChange={(e) => handleChange({ name: e.target.value })}
             />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+            <input
+              placeholder="Industry"
+              value={payload.industry}
+              onChange={(e) => handleChange({ industry: e.target.value })}
+            />
+            <input
+              placeholder="Email"
+              type="email"
+              value={payload.email}
+              onChange={(e) => handleChange({ email: e.target.value })}
+            />
+            <input
+              placeholder="Phone Number"
+              type="tel"
+              value={payload.phoneNumber}
+              onChange={(e) => handleChange({ phoneNumber: e.target.value })}
+            />
+            <button onClick={launchWhatsAppSignup} type="button">
+              Sign up
+            </button>
+          </form>
+        </main>
+      </div>
+
+      <script
+        async
+        defer
+        crossOrigin="anonymous"
+        src="https://connect.facebook.net/en_US/sdk.js"
+      ></script>
+    </>
+  )
 }
